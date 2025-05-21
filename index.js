dc49182acec05ddd84232505e9121ed3b4f9f744
Belal <derbelalrahimi@gmail.com>
2025-05-21 20:28:12 +0200

    worked a bit with variables, strings and boolean

--- index.js
+++ index.js
@@ -3,4 +3,20 @@ console.log("Hello dear");
 document.getElementById("h1").textContent = "nice";
 document.getElementById("p").textContent = "great";
 
-window.alert("welcome to my website");
+//window.alert("welcome to my website");
+
+let vorname = "Belal";
+let age = 24;
+let country = "";
+let isGamer = true;
+
+console.log(`My name is: ${vorname}`);
+console.log(`My age is: ${age}`);
+console.log(`Im from ${country}`);
+console.log(typeof country);
+console.log(`Im a gamer: ${isGamer}`);
+
+document.getElementById("h2").textContent = `Ich bin ${vorname}`;
+document.getElementById("p1").textContent = `Ich bin ${age} jahre alt`;
+document.getElementById("p2").textContent = `Ich komme aus Afg ${country}`;
+document.getElementById("p3").textContent = `Bin ich ein gamer? ${isGamer}`;
