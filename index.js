5aabb9369f102139017cdf1bc4bb4a39d33ad177
Belal <derbelalrahimi@gmail.com>
2025-05-21 21:08:14 +0200

    input, button and labels added

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ let age = 24;
 let country = "";
 let isGamer = true;
 
+let programs = 5;
+
+//programs += 1;
+//programs -= 1;
+//programs *= 2;
+//programs /= 2;
+//programs %= 2;
+programs **= 2;
+
+programs++;
+
 console.log(`My name is: ${vorname}`);
 console.log(`My age is: ${age}`);
 console.log(`Im from ${country}`);
@@ -20,3 +31,12 @@ document.getElementById("h2").textContent = `Ich bin ${vorname}`;
 document.getElementById("p1").textContent = `Ich bin ${age} jahre alt`;
 document.getElementById("p2").textContent = `Ich komme aus Afg ${country}`;
 document.getElementById("p3").textContent = `Bin ich ein gamer? ${isGamer}`;
+
+console.log(programs);
+
+let playerName;
+
+document.getElementById("btn").onclick = function(){
+    playerName = document.getElementById("input").value;
+    document.getElementById("h1").textContent = `Welcome ${playerName}`;
+}
