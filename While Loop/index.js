d11115ed752c03ab19ea956b0e94ca232cda2edc
Belal <derbelalrahimi@gmail.com>
2025-05-24 13:34:56 +0200

    worked with for loops

--- While Loop/index.js
+++ While Loop/index.js
@@ -15,7 +15,7 @@ while(!isLoggedIn){
     else{
         console.log("either ID or password is invalid!");
     }
-}*/
+}
 //do while; checking if the result of two numbers is even or odd
 let x;
 let y;
@@ -37,4 +37,26 @@ if(result % 2 === 0){
 }
 else{
         console.log("The totla of X and Y is an odd number");
+}*/
+//for loop
+for(let i = 0; i <= 10; i++){
+    console.log(i);
 }
+console.log("great job!");
+
+for(let x = 10; x > 0; x--){
+    console.log(x)
+}
+console.log("congrats!");
+
+for(y = 0; y <=10; y++){
+    if(y==6){
+        continue;
+    }
+    else if(y == 7){
+        break;
+    }
+    console.log(y);
+}
+console.log("6 is skipped and we stopped at 7");
+
