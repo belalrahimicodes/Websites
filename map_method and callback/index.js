11ddd65e2240440281ae12a200dd63a62b10ad08
Belal <derbelalrahimi@gmail.com>
2025-05-25 21:05:57 +0200

    filter method added

--- map_method and callback/index.js
+++ map_method and callback/index.js
@@ -27,3 +27,42 @@ function updateDates(element){
     const sections = element.split("/");
     return `Updated date: ${sections[2]} - ${sections[1]} - ${sections[0]}`;
 }
+
+//filter method
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+//filtering even numbers from an array of numbers
+const evenNumbers = numbers.filter(isEven);
+console.log(`Even numbers filtered: ${evenNumbers}`);
+
+function isEven(element){
+    return element % 2 === 0;
+}
+
+//filtering odd numbers from an array of numbers
+const oddNumbers = numbers.filter(isOdd);
+console.log(`Odd numbers filtered: ${oddNumbers}`);
+
+function isOdd(element){
+    return element % 2 !== 0;
+}
+
+//filtering adults from an array of different ages
+const ages = [12, 14, 88, 34, 18, 90, 23];
+const onlyAdults = ages.filter(adults);
+
+console.log(`Adults filtered: ${onlyAdults}`);
+
+function adults(element){
+    return element >= 18;
+}
+
+//filtering children from an array of different ages
+const onlyChildren = ages.filter(children);
+
+console.log(`Children filtered: ${onlyChildren}`);
+
+function children(element){
+    return element < 18;
+}
