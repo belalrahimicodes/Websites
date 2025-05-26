923914f27e77f55117aebf4d259fe62d0fb87123
Belal <derbelalrahimi@gmail.com>
2025-05-26 08:15:02 +0200

    reduce method and function expression added

--- map_method and callback/index.js
+++ map_method and callback/index.js
@@ -66,3 +66,52 @@ console.log(`Children filtered: ${onlyChildren}`);
 function children(element){
     return element < 18;
 }
+
+//reduce method
+
+const grades = [23, 34, 45, 67, 12, 77];
+
+const sumOf = grades.reduce(add);
+
+console.log(`Sum of numbers is: ${sumOf}`);
+
+function add(accumulator, element){
+    return accumulator + element;
+}
+
+const maxOf = grades.reduce(max);
+
+console.log(`The max number is: ${maxOf}`);
+
+function max(accumulator, element){
+    return Math.max(accumulator, element);
+}
+
+//function expressions
+
+setTimeout(function(){
+    console.log(`hey man`)
+}, 2000);
+
+const nums = [2, 3, 4, 5, 6];
+
+const cubes = nums.map(function(element){
+    return Math.pow(element, 3);
+});
+console.log(`cubes: ${cubes}`);
+
+const squares = nums.map(function(element){
+    return Math.pow(element, 2);
+});
+console.log(`squares: ${squares}`);
+
+const evenNums = nums.filter(function(element){
+    return element % 2 === 0;
+});
+
+console.log(`Even numbers filtered: ${evenNums}`);
+
+const addNums = nums.reduce(function(accumulator, element){
+    return accumulator + element;
+});
+console.log(`sum of nums: ${addNums}`);
