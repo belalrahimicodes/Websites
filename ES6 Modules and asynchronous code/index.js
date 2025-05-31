40287228954a3ef7e575464089c6bddf8c82ee2d
Belal <derbelalrahimi@gmail.com>
2025-05-31 12:43:12 +0200

    Error handling added

--- ES6 Modules and asynchronous code/index.js
+++ ES6 Modules and asynchronous code/index.js
@@ -29,3 +29,37 @@ function task2(){
 }
 
 task1(task2);
+
+//Error handling
+
+try{
+    console.log(a);
+}
+catch(error){
+    console.error(error);
+}
+finally{
+    console.log("This part of the code in error handling always runs.");
+}
+
+//ex2
+
+try{
+    const num1 = Number(window.prompt("Enter your first number:"));
+    const num2 = Number(window.prompt("Enter your second number:"));
+    
+    if(num2 == 0){
+        throw new Error("Your number can't be divided by 0!");
+    }
+    if(isNaN(num1) || isNaN(num2)){
+        throw new Error("Please enter a number!");
+    }
+    const result = num1 / num2;
+    console.log(result);
+}
+catch(error){
+    console.error(error);
+}
+finally{
+    console.log("This code always runs.");
+}
